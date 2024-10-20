2bf7f18e7d1c3c6bd085ed8b1d27b8af1a49f4c4
Nikita Daga <nmd43@duke.edu>
2024-10-20 17:08:46 -0400

    updated set up reqs

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Open Project
 
+Run these commands: 
+
+pip install -r requirements.txt
+
+
+
 # Roamio
 Personalized travel itinerary builder that connects friends and allows people to identify the best activities and places to visit.
 
 # Team Members 
 Ananya Agrawal, Aryaman Dalal, Nikita Daga, Divyansh Jain, Shawn Ma
-
-# Milestone 2 Progress
 
 
 
--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+# File: /path/to/your/project/config.py
+   # or
+   # File: /path/to/your/project/config/config.py
+
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:roamiopass@localhost:5432/roamio'
+        'postgresql://postgres:roamiopass@172.28.237.236:5432/roamio'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
