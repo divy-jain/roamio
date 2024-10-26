b86f1799dd4c3913d8601872789c8d238313b44f
shawnsma <shenglongma09@gmail.com>
2024-10-26 16:27:59 -0400

    Addition to Login

--- config.py
+++ config.py
@@ -7,15 +7,16 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:roamiopass@roamio.czugw66qwqxb.us-east-2.rds.amazonaws.com:5432/roamio'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:roamiopass@roamio.czugw66qwqxb.us-east-2.rds.amazonaws.com:5432/roamio'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "connect_args": {"sslmode": "require"}
     }
 
     # Secret key for the application
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'you-will-never-guess'
 
     # Flask-Mail configuration
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
